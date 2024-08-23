51678878a49ca28c3e2864a8b0bbb9f0e7b4ea53
Arun Shankar <arunprshankar@gmail.com>
2024-08-22 22:38:01 -0500

    wip

--- src/generate/llm.py
+++ src/generate/llm.py
@@ -91,6 +91,11 @@ def generate_response(model: GenerativeModel, contents: List[Part], response_sch
         raise
 
 
+def load_template(template_path):
+    with open(template_path, 'r') as file:
+        template_content = file.read()
+    return template_content
+
 
 def load_and_fill_template(template_path: str, topic: str) -> str:
     """
@@ -110,6 +115,35 @@ def load_and_fill_template(template_path: str, topic: str) -> str:
             
             # Replace all occurrences of '{}' with the topic
             filled_template = template_content.replace("{}", topic)
+            
+            logger.info(f"Filled template content: {filled_template}")
+            return filled_template
+    except FileNotFoundError as e:
+        logger.error(f"Template file not found: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Error loading and filling template: {e}")
+        raise
+
+
+def load_and_fill_template2(template_path: str, article: str) -> str:
+    """
+    Load a template from a file and replace all occurrences of {} with the topic.
+
+    Args:
+        template_path (str): The path to the template file.
+        topic (str): The AI concept to insert into the template.
+
+    Returns:
+        str: The filled template as a string.
+    """
+    try:
+        with open(template_path, 'r') as file:
+            template_content = file.read()
+            logger.info(f"Loaded template content: {template_content}")
+            
+            # Replace all occurrences of '{}' with the topic
+            filled_template = template_content.replace("{}", article)
             
             logger.info(f"Filled template content: {filled_template}")
             return filled_template
--- src/patterns/actor_critic/generate/generator.py
+++ src/patterns/actor_critic/generate/generator.py
@@ -1,46 +1,101 @@
 from vertexai.generative_models import GenerativeModel
-from src.generate.llm import load_and_fill_template
-from src.generate.llm import generate_response
-from src.generate.llm import load_json
-from src.config.logging import logger 
+from src.generate.llm import (
+    load_and_fill_template,
+    load_and_fill_template2,
+    load_template,
+    generate_response,
+    load_json,
+)
+from src.config.logging import logger
 from src.config.setup import config
+import json
 
 
-def generate_draft(topic: str) -> None:
+def generate_draft(topic: str) -> str:
     """
-    Generate and save content using a generative model based on a given topic.
+    Generate content using a generative model based on a given topic.
+
+    Args:
+        topic (str): The topic to generate content for.
+
+    Returns:
+        str: The generated article.
 
     Raises:
         Exception: If any error occurs during the content generation process, it is logged and re-raised.
     """
     try:
-        logger.info("Starting LLM extraction")
-        system_instruction = load_and_fill_template('./data/patterns/actor_critic/actor/write/system_instructions.txt', topic)
-        print(system_instruction)
-        user_instruction = load_and_fill_template('./data/patterns/actor_critic/actor/write/user_instructions.txt', topic)
+        logger.info("Starting LLM extraction for topic: %s", topic)
+        system_instruction = load_and_fill_template(
+            './data/patterns/actor_critic/actor/write/system_instructions.txt', topic
+        )
+        user_instruction = load_and_fill_template(
+            './data/patterns/actor_critic/actor/write/user_instructions.txt', topic
+        )
         response_schema = load_json('./data/patterns/actor_critic/actor/write/response_schema.json')
+
         if response_schema is None:
             raise ValueError("Response schema could not be loaded.")
-        
+
         model = GenerativeModel(config.TEXT_GEN_MODEL_NAME, system_instruction=system_instruction)
         contents = [user_instruction]
         response = generate_response(model, contents, response_schema)
-        print(response['article'])
+        article = response['article']
+        logger.info("Article generation successful.")
+        return article
+
     except Exception as e:
-        logger.error(f"Error in LLM extraction: {e}")
+        logger.error("Error in LLM extraction: %s", e)
         raise
 
-def review_draft() -> None:
-    system_instruction = load_and_fill_template('./data/patterns/actor_critic/actor/write/system_instructions.txt', topic)
-    user_instruction = load_and_fill_template('./data/patterns/actor_critic/actor/write/user_instructions.txt', topic)
-    response_schema = load_json('./data/patterns/actor_critic/actor/write/response_schema.json')
-    if response_schema is None:
-        raise ValueError("Response schema could not be loaded.")
-    
-    model = GenerativeModel(config.TEXT_GEN_MODEL_NAME, system_instruction=system_instruction)
-    contents = [user_instruction]
-    response = generate_response(model, contents, response_schema)
-    print(response['article'])
+
+def review_draft(article: str) -> dict:
+    """
+    Review the generated article using a generative model.
+
+    Args:
+        article (str): The article to be reviewed.
+
+    Returns:
+        dict: The response containing the review.
+
+    Raises:
+        ValueError: If the response schema could not be loaded.
+    """
+    try:
+        logger.info("Starting review process for the generated article.")
+        system_instruction = load_template('./data/patterns/actor_critic/critic/review/system_instructions.txt')
+        user_instruction = load_and_fill_template2(
+            './data/patterns/actor_critic/critic/review/user_instructions.txt', article=article
+        )
+        response_schema = load_json('./data/patterns/actor_critic/critic/review/response_schema.json')
+
+        if response_schema is None:
+            raise ValueError("Response schema could not be loaded.")
+
+        model = GenerativeModel(config.TEXT_GEN_MODEL_NAME, system_instruction=system_instruction)
+        contents = [user_instruction]
+        response = generate_response(model, contents, response_schema)
+        logger.info("Review generation successful.")
+        return response
+
+    except Exception as e:
+        logger.error("Error in review process: %s", e)
+        raise
+
 
 if __name__ == "__main__":
-    pass
+    try:
+        article = generate_draft('perplexity')
+        logger.info("Generated Article: %s", article)
+        print("Generated Article:")
+        print(json.dumps(article, indent=4))
+        print('-' * 100)
+
+        review = review_draft(article)
+        logger.info("Generated Review: %s", review)
+        print("Generated Review:")
+        print(json.dumps(review, indent=4))
+
+    except Exception as e:
+        logger.error("An error occurred in the main execution: %s", e)
